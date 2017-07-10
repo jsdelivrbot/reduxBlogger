21912841747fe1ccb48fcdd4f5aafcb989969bf4
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 17:41:41 -0700

    Connect PostsIndex to state, render basic list

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    _.map(this.props.posts, post => (
+      <li className='list-group-item' key={post.id}>
+        {post.title}
+      </li>
+    ));
+  }
+
   render() {
     return(
       <div>
-        PostsIndex
+        <h3>Posts</h3>
+        <ul className='list-group'>
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default PostsIndex;
+const mapStateToProps = ({ posts }) => ({ posts });
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
