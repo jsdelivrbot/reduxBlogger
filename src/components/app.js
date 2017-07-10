ea6589b4057e1dd48c39f6d6923fce7a6c005b71
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 17:57:25 -0700

    Add link to /posts/new on index page

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PostsIndex from './posts_index';
 import PostsNew from './posts_new';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Route path="/" component={PostsIndex} />
+      <Switch>
         <Route path="/posts/new" component={PostsNew} />
-      </div>
+        <Route path="/" component={PostsIndex} />
+      </Switch>
     );
   }
 }
