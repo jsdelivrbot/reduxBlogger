ed1527c9d7e6468093043058253d538011bde6c6
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 18:27:34 -0700

    Refactor renderField, add tags and content fields to reduxForm

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return(
-      <input
-        type='text'
-        {...field.input}
-      />
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+      </div>
     );
   }
 
@@ -16,7 +20,18 @@ class PostsNew extends Component {
       <form>
         <Field
           name='title'
-          component={this.renderTitleField}
+          label='Title'
+          component={this.renderField}
+        />
+        <Field
+          name='tags'
+          label='Tags'
+          component={this.renderField}
+        />
+        <Field
+          name='content'
+          label='Post Content'
+          component={this.renderField}
         />
 
       </form>
