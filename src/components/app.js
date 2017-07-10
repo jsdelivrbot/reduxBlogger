8255097e684c4e8f74dfaa6e1d75162553d92ded
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 17:47:04 -0700

    Add /posts/new route and basic component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import PostsIndex from './posts_index';
+import PostsNew from './posts_new';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
     );
   }
