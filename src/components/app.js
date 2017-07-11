55e6f2c40d6b7b1d6b5e8e7c5c8bad91233a0baa
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-11 11:00:06 -0700

    Add route to PostsShow component

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PostsIndex from './posts_index';
 import PostsNew from './posts_new';
+import PostsShow from './posts_show';
 
 export default class App extends Component {
   render() {
     return (
       <Switch>
         <Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsShow} />
         <Route path="/" component={PostsIndex} />
       </Switch>
     );
