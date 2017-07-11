9e3087bf7bde4d40ed7740e10f51f14ae675e7f5
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-11 11:29:17 -0700

    Add delete button on PostsShow page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
@@ -9,6 +9,11 @@ class PostsShow extends Component {
       const { id } = this.props.match.params;
       this.props.fetchPost(id);
     }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id);
   }
 
   render() {
@@ -21,6 +26,11 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to='/'>Back To Index</Link>
+        <button
+          className='btn btn-danger pull-xs-right'
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -33,4 +43,7 @@ const mapStateToProps = ({ posts }, { match }) => (
   { post: posts[match.params.id] }
 );
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(
+  mapStateToProps,
+  { fetchPost, deletePost }
+)(PostsShow);
