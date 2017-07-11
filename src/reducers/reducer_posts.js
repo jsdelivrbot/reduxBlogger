9d2e4144cada3f7c53150e9f3163953156dff46d
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-11 11:07:33 -0700

    Handle fetchPost in posts reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
+  case FETCH_POST:
+    const post = action.payload.data
+    return { ...state, [post.id]: post };
   default:
     return state;
   }
